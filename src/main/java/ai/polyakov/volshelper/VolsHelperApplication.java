c7e8e6a23a10131b5c8f678c76d386055eb4c663
Alexey Polyakov <sulfur1917@gmail.com>
2023-09-10 20:28:03 +0300

    Start project.

---
+++ src/main/java/ai/polyakov/volshelper/VolsHelperApplication.java
@@ -0,0 +1,13 @@
+package ai.polyakov.volshelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolsHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolsHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/polyakov/volshelper/VolsHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.polyakov.volshelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolsHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
