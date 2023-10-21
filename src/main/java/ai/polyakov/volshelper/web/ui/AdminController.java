61b26f8d9380b09d3ceadfbe300aae1c632ff84d
Alexey Polyakov <sulfur1917@gmail.com>
2023-10-21 22:00:11 +0300

    Line schemes between markers.

--- src/main/java/ai/polyakov/volshelper/web/ui/AdminController.java
+++ src/main/java/ai/polyakov/volshelper/web/ui/AdminController.java
@@ -1,15 +1,9 @@
 package ai.polyakov.volshelper.web.ui;
 
 import ai.polyakov.volshelper.model.Line;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 @Controller
 @RequestMapping("/admin")
--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -3,25 +3,58 @@ let line = null;
 let draw = null;
 let form = null;
 
-// undo event of keypress 'ctrl + z'
-document.onkeydown = KeyPress;
-
 $(function () {
     form = $('#addLine');
     line = new Line();
 });
 
+const typePoint = {BASE:'base', COUPLING:'coupling'}
+
 class Line {
     constructor() {
-        this._coordinates = [];
+        this._points = [];
+        this._markers = [];
     }
 
     get coordinates() {
-        return this._coordinates;
+        return this._points;
     }
 
     set coordinates(value) {
-        this._coordinates = value;
+        this._points = value;
+    }
+
+}
+
+class Point {
+    constructor(lat, lng, typePoint) {
+        this._lat = lat;
+        this._lng = lng;
+        this._typePoint = typePoint;
+    }
+
+    get lat() {
+        return this._lat;
+    }
+
+    set lat(value) {
+        this._lat = value;
+    }
+
+    get lng() {
+        return this._lng;
+    }
+
+    set lng(value) {
+        this._lng = value;
+    }
+
+    get typePoint() {
+        return this._typePoint;
+    }
+
+    set typePoint(value) {
+        this._typePoint = value;
     }
 }
 
@@ -37,17 +70,20 @@ function openMap() {
 function closeMap() {
     /*draw = null;*/
 }
-function pencil() {
+
+function pencil(typePoint) {
     L.DomUtil.addClass(draw._container, 'crosshair-cursor-enabled');
-    polyLine = L.polyline(line._coordinates, {color: 'red'}).addTo(draw);
+    if (polyLine === null) {
+        polyLine = L.polyline([], {color: 'red'}).addTo(draw);
+    }
     draw.on('click', function (e) {
         let coord = e.latlng;
         let lat = coord.lat;
         let lng = coord.lng;
-        if (line._coordinates.length < 1) {
-            pushCoordinates(lat, lng, line);
+        if (line._points.length < 1) {
+            pushCoordinates(lat, lng, typePoint);
         } else {
-            pushCoordinates(lat, lng, line);
+            pushCoordinates(lat, lng, typePoint);
             redraw();
 
 // zoom the map to the polyline
@@ -56,27 +92,41 @@ function pencil() {
     });
 }
 
-function redraw() {
-    polyLine.setLatLngs(line._coordinates);
-}
-
 function undo() {
-    if (line._coordinates.length > 0) {
-        line._coordinates.pop();
+    if (line._points.length > 0) {
+        line._points.pop();
+        let removeMarker = line._markers.pop();
+        removeMarker.onRemove(draw);
         redraw();
     }
 }
+
+function redraw() {
+    let coordinates = [];
+    line._points.forEach((point) => {
+        coordinates.push([point._lat, point._lng]);
+    })
+    polyLine.setLatLngs(coordinates);
+}
+
+function pushCoordinates(lat, lng, typePoint) {
+    let newPoint = new Point(lat, lng, typePoint);
+    let newMarker = L.marker([lat, lng], {draggable: true}).addTo(draw);
+    newMarker.on('drag', function(e){
+        line.setLatLngs([newMarker.getLatLng(), newMarker.getLatLng()]);
+    });
+    line._points.push(newPoint);
+    line._markers.push(newMarker);
+}
+
+
+function getForm() {
+    return form;
+}
+// undo event of keypress 'ctrl + z'
+document.onkeydown = KeyPress;
 function KeyPress(e) {
     let evtobj = window.event? event : e
     if (evtobj.keyCode === 90 && evtobj.ctrlKey) undo();
-}
-function pushCoordinates(lat, lng, line) {
-    let arr = line._coordinates;
-    arr.push([lat, lng]);
-    line._coordinates = arr;
-    return line;
-}
-function getForm() {
-    return form;
 }
 
